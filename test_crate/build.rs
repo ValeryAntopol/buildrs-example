80a2fec6680f413cf45d311a99793f03c5a281f8
Valery Antopol <valery.antopol@gmail.com>
2021-11-03 17:38:20 +0300

    Add struct with impl: plugin does not parse impl

--- test_crate/build.rs
+++ test_crate/build.rs
@@ -3,7 +3,15 @@ use std::{fs, path, env};
 fn main() {
     let content = "\
     pub mod test_mod {
-        pub fn test_fun() {}
+        pub struct TestStruct3 {
+            pub test_field: i32,
+        }
+        impl TestStruct3 {
+            pub fn new() -> Self {Self{test_field: 0,}}
+            pub fn test_method() {}
+            pub fn test_method_self(&self) -> &Self {&self}
+        }
+        pub fn test_func() {}
     }
     ";
 
--- test_crate/src/main.rs
+++ test_crate/src/main.rs
@@ -1,4 +1,6 @@
 include!(concat!(env!("OUT_DIR"), "/test_mod.rs"));
 fn main() {
-    test_mod::test_fun();
+    let x: test_mod::TestStruct3 = test_mod::TestStruct3::new();
+    x.test_method_self();
+    x.test_field;
 }
